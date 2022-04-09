9750c30a7bf95df163d4a51f2380af111bb2b3af
gnii <chochain@yahoo.com>
2022-04-09 16:10:15 -0700

    ~/src/div_int.sv add rx back for 65-bit ops

--- source/div_int.sv
+++ source/div_int.sv
@@ -9,17 +9,15 @@ module div_int #(parameter DSZ=32) (
     output logic [DSZ-1:0] r          // remainder
     );
     logic [DSZ-1:0] _q;               // intermediate quotient
-    logic [DSZ:0]   _r;               // accumulator (1 bit wider)
+    logic [DSZ:0]   _r, rx;           // accumulator (1 bit wider)
     logic [$clog2(DSZ)-1:0] i;        // iteration counter
 
     always_comb begin
-        if (r >= y) begin
-            _r = r - y;               
-            {_r, _q} = ({_r, q} << 1) | 1'b1;  // 65-bit ops
+        if (rx >= {1'b0, y}) begin
+            _r = rx - y;
+            {_r, _q} = {_r[DSZ-1:0], q, 1'b1};   // 65-bit ops
         end
-        else begin
-            {_r, _q} = {1'b0, r, q} << 1;
-        end 
+        else {_r, _q} = {rx[DSZ-1:0], q, 1'b0};
     end
 
     always_ff @(posedge clk) begin
@@ -27,17 +25,17 @@ module div_int #(parameter DSZ=32) (
             i      <= 0;
             busy   <= y != 0;
             dbz    <= y == 0;
-            {r, q} <= {{(DSZ-1){1'b0}}, x, 1'b0};
+            {rx, q} <= {{DSZ{1'b0}}, x, 1'b0};
         end
         else if (busy) begin
-            if (i != DSZ-1) r <= _r;
-            else begin
+            if (i == (DSZ-1)) begin
                 busy <= 0;             // are we done?
                 r    <= _r[DSZ:1];     // undo final shift
             end
+            else rx  <= _r;
             q <= _q;
-            i <= i + 1;                // advance loop counter
-            // $write("[%d] _q,_r=%8x,%8x ", i, _q, _r);
+            i <= i + 1;
+            $write("[%d] y,_q,_r=%8x,%8x,%8x ", i, y, _q, _r);
         end
     end
 endmodule: div_int
