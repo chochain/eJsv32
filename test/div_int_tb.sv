728a96a0936e094df584657aa05e5b6d0181eb14
gnii <chochain@yahoo.com>
2022-04-10 16:35:37 -0700

    ~/test/div_int_tb add test vector driver

--- test/div_int_tb.sv
+++ test/div_int_tb.sv
@@ -6,16 +6,16 @@ module div_int_tb #(
     parameter DSZ  = 32        /* 32-bit data width  */
     );
 
-    logic          clk, rst;
-    logic          div_rst, div_busy, div_by_z;
-    logic[DSZ-1:0] div_q, div_r;
+    logic          clk;
     logic[1:0]     phase;
-    logic[DSZ-1:0] s = 'h20;
-    logic[DSZ-1:0] t = 'h10;
+    logic          div_rst, div_busy, div_by_z;
+    logic[DSZ-1:0] s, t, div_q, div_r;
+
+    logic[DSZ-1:0] s_vec[] = { 'h1, 'h5, 'hA, 'hC };
 
     div_int  #(DSZ) divide_inst (
     .clk(clk),
-    .rst(rst),
+    .rst(div_rst),
     .x(s),
     .y(t),
     .busy(div_busy),
@@ -25,25 +25,40 @@ module div_int_tb #(
     );
 
     task DIV();
+        //$display("%6t>%cdiv[%d] %8x,%8x", $time, div_busy ? "*" : " ", phase, div_r, div_q);
         case (phase)
-          0: begin phase = 1; rst = 1'b0; end
-          1: begin phase = div_busy ? 1 : 2; rst = 1'b0; end
-          default: begin phase = 0; rst = 1'b1; end
+          0: begin phase = 1; div_rst = 1'b1; end
+          default: begin
+              if (div_busy) begin phase = 1; div_rst = 1'b0; end
+              else begin phase = 0; div_rst = 1'b1; end
+          end
         endcase
-        $display("%6t>%c%d %8x,%8x", $time, div_busy ? "*" : " ", phase, div_q, div_r);
     endtask: DIV
+
+    task one_loop(integer n, [DSZ-1:0] t_vec);
+        for (int i=0; i<4; i++) begin
+            phase   = 0;
+            div_rst = 1'b1;
+            s       = (s_vec[i] << n) | s_vec[i];
+            t       = t_vec << n;
+            $write("case %x / %x => ", s, t);
+            do repeat(1) @(posedge clk)
+                DIV();
+            while (phase > 0);
+            $display("%8x..%8x", div_q, div_r);
+            assert(div_r == (s % t));
+            assert(div_q == (s / t));
+        end
+    endtask: one_loop
 
     always #5 clk  = ~clk;
 
     initial begin
-        clk   = 1'b0;         // start clock
-        rst   = 1'b1;         // enable divider
-        phase = 0;
+        clk = 1'b0;         // start clock
 
-        repeat(40) @(posedge clk) begin
-           DIV();
+        for (int j=0; j<DSZ; j++) begin
+            one_loop(j, 'h1);
         end
-        rst = 1'b1;           // disable divider
 
         #20 $finish;
     end
