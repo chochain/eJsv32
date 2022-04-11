32da1735b134db12463cee7c99ac4c5496bd8730
gnii <chochain@yahoo.com>
2022-04-10 19:40:04 -0700

    ~/test/outer_tb untabify; rm whitespaces

--- test/outer_tb.sv
+++ test/outer_tb.sv
@@ -15,7 +15,7 @@ module outer_tb #(
     localparam SSZ = $clog2(SS_DEPTH);
     localparam RSZ = $clog2(RS_DEPTH);
     localparam DOT = 'h2e;
-   
+
     logic [7:0]      data_o_i, data_o_o, code_o;
     logic [DSZ-1:0]  t_o;
     logic [ASZ-1:0]  addr_o_o, p_o, a_o;
@@ -26,13 +26,13 @@ module outer_tb #(
 
     logic            clk, rst;
     logic [ASZ-1:0]  ctx, here;
-   
+
     mb8_io      b8_if();
     spram8_128k m0(b8_if.slave, ~clk);
 
     dict_setup  #(MEM0, TIB) dict(.*, .b8_if(b8_if.master));
     eJ32        #(TIB, OBUF, DSZ, ASZ, SS_DEPTH, RS_DEPTH) u(.clk, .clr(rst), .*);
-    
+
     task at([ASZ-1:0] ax, [1:0] opt);
         repeat(1) @(posedge clk) begin
             case (opt)
@@ -56,19 +56,19 @@ module outer_tb #(
             at(i, 'h2);
         end
     endtask: dump_row
-    
+
     task dump(input [ASZ-1:0] addr, input [ASZ-1:0] len);
         automatic logic [ASZ-1:0] a0 = addr & ~'hf;
         for (integer a1=a0; a1 < (a0 + len + 'h10); a1 += 'h10) begin
             dump_row(a1);
         end
     endtask: dump
-   
-    task verify_tib; 
+
+    task verify_tib;
         $display("\ndump tib %04x", TIB);
-        dump(TIB, 'h120);
+        dump('h400, 'h100);
     endtask: verify_tib;
-    
+
     task verify_obuf;
         $display("\ndump obuf %04x", OBUF);
         dump(OBUF, 'h100);
@@ -81,26 +81,26 @@ module outer_tb #(
     endtask: activate
 
     always #5 clk  = ~clk;
-       
+
     assign data_o_i = b8_if.vo;
-   
+
     always_comb begin
         if (write_o) b8_if.put_u8(addr_o_o, data_o_o);
         else         b8_if.get_u8(addr_o_o);
     end
-       
+
     initial begin
         clk = 1'b0;           // start clock
         rst = 1'b1;           // disable eJsv32
-      
+
         dict.setup_mem();     // fill dictionary from hex file
         activate();           // activate eJsv32
-        repeat(2000) @(posedge clk);
+        repeat(20000) @(posedge clk);
         rst = 1'b1;           // disable eJsv32
-        
+
         verify_tib();         // validate input buffer content
         verify_obuf();        // validate output buffer content
-        
+
         #20 $finish;
     end
 endmodule: outer_tb
