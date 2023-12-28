bc7e7c93be62bddb7a5e0870d0bdeaac35cd0ef0
gnii <chochain@yahoo.com>
2023-12-28 15:51:32 +0800

    ~/src/eJ32_ls.sv drop code_x,phase_n; mv ALU, ss_op to AU

--- source/eJ32_ls.sv
+++ source/eJ32_ls.sv
@@ -3,10 +3,6 @@
 //
 `include "../source/eJ32_if.sv"
 
-`define PHASE0 phase_n = 0
-`define R(op)  ctl.rs_op = op
-`define S(op)  ctl.ss_op = op
-
 module EJ32_LS #(
     parameter TIB      = 'h1000,          ///> input buffer address
     parameter OBUF     = 'h1400,          ///> output buffer address
@@ -26,7 +22,6 @@ module EJ32_LS #(
     /// @defgroup Registers
     /// @{
     // instruction
-    `U3  phase;                 ///> FSM phase (aka state)
     `IU  a;                     ///> memory address
     `IU  asel;
     // IO
@@ -35,13 +30,9 @@ module EJ32_LS #(
     /// @}
     /// @defgroup Next Register
     /// @{
-    // instruction
-    opcode_t code_n;            ///> JVM opcode
-    `U3 phase_n;                ///> FSM phase (aka state)
+    `DU t_n;                    ///> TOS
     `IU a_n;                    ///> data address
     `IU asel_n;                 ///> address select
-    `DU t_n;                    ///> TOS
-    `DU r_n;                    ///> top of return stack
     `U8 data_n;
     `U2 dsel_n;                 ///> 32-bit, 4-to-1 mux, byte select
     /// @}
@@ -49,12 +40,12 @@ module EJ32_LS #(
     /// @{
     // instruction
     opcode_t code;              ///> shadow ctl.code
-    `U1 code_x;                 ///> instruction unit control
-    `U1 p_x, a_x;               ///> address controls
+    `U3  phase;                 ///> FSM phase (aka state)
+    `U1 a_x;                    ///> address controls
     `IU a_d;                    ///> 2-byte merged address
     // data stack
     `DU t;                      ///> shadow TOS
-    `U1 t_x, t_z, t_neg;        ///> TOS controls
+    `U1 t_x;
     `DU t_d;                    ///> 4-byte merged data
     // memory & IO buffers
     `U8 d8x4[4];                ///> 4-to-1 byte select
@@ -62,23 +53,16 @@ module EJ32_LS #(
     `U1 ibuf_x, obuf_x;         ///> input/output buffer controls
     /// @}
 
-    // data stack
-    task TOS(input `DU v);  t_n = v; `SET(t_x);  endtask;
-    task ALU(input `DU v);  TOS(v); `S(sPOP);    endtask;
-    task PUSH(input `DU v); TOS(v); `S(sPUSH);   endtask;
-    task POP();             TOS(s); `S(sPOP);    endtask;
-    // memory unit
-    task SETA(input `IU a); a_n = a; `SET(a_x);   endtask;      // build addr ptr
-    task MEM(input `IU a); SETA(a); `SET(asel_n); endtask;   // fetch from memory, data returns next cycle
-    task DW(input `U3 n); dsel_n = n; `SET(dwe); `SET(dsel_x); endtask;   // data write n-th byte
+    task TOS(input `DU v);  t_n = v; `SET(t_x);   endtask;   ///> update TOS
+    task SETA(input `IU a); a_n = a; `SET(a_x);   endtask;   ///> build addr ptr
+    task MEM(input `IU a); SETA(a); `SET(asel_n); endtask;   ///> fetch from memory, data returns next cycle
+    task DW(input `U3 n); dsel_n = n; `SET(dwe); `SET(dsel_x); endtask;   ///> data write n-th byte
     ///
     /// wires to reduce verbosity
     ///
     assign code     = ctl.code;               ///> input from ej32 control
     assign phase    = ctl.phase;
     assign t        = ctl.t;
-    assign t_z      = ctl.t_z;
-    assign t_neg    = ctl.t_neg;
     assign ls_addr_o= a;                      ///> memory address
     assign ls_asel_o= asel;
     assign data_o   = data_n;                 ///> data sent to memory bus
@@ -95,18 +79,16 @@ module EJ32_LS #(
     /// combinational
     ///
     task INIT();
-        code_x    = 1'b1;         /// fetch opcode by default
+        t_n       = {DSZ{1'b0}};  /// TOS
+        t_x       = 1'b0;
         a_n       = {ASZ{1'b0}};  /// default to clear address
         a_x       = 1'b0;
         asel_n    = 1'b0;         /// address default to program counter
-        t_n       = {DSZ{1'b0}};  /// TOS
-        t_x       = 1'b0;
         dsel_x    = 1'b0;         /// data bus
         dsel_n    = 3;
         dwe       = 1'b0;         /// data write
         ibuf_x    = 1'b0;
         obuf_x    = 1'b0;
-        ctl.ss_op = sNOP;         /// data stack
     endtask: INIT
 
     always_comb begin
@@ -133,54 +115,44 @@ module EJ32_LS #(
             endcase
         iastore:
             case (phase)
-            0: begin MEM(`XDA(s)); `S(sPOP); `SET(dsel_x); dsel_n = 0; end
+            0: begin MEM(`XDA(s)); `SET(dsel_x); dsel_n = 0; end
             1: begin MEM(a + 1); DW(1); end
             2: begin MEM(a + 1); DW(2); end
             3: begin MEM(a + 1); DW(3); end
-            4: begin DW(3); POP(); end         // CC: reset a?
+            4: DW(3);                          // CC: reset a?
             endcase
         bastore:
             case (phase)
-            0: begin MEM(`XDA(s)); `S(sPOP); end
-            1: begin POP(); DW(3); end         // CC: reset a?
+            0: MEM(`XDA(s));
+            1: DW(3);                          // CC: reset a?
             endcase
         sastore:
-            case (phase)
-            // CC: logic changed
-            // 0: begin WAIT(1); MEM(s); `SET(spop); end
-            // 1: begin WAIT(2); MEM(a + 1); DW(2); end
-            // 2: begin WAIT(3); POP(); DW(3); `SET(asel_n); end
-            0: begin MEM(`XDA(s)); `S(sPOP); `SET(dsel_x); dsel_n = 2; end
+            case (phase)                       // CC: logic changed from Dr. Ting's
+            0: begin MEM(`XDA(s)); `SET(dsel_x); dsel_n = 2; end
             1: begin MEM(a + 1); DW(3); end
-            2: begin DW(3); POP(); end
-            default: `PHASE0;
+            2: DW(3);
             endcase
         iinc:
+            case (phase)                       // CC: logic changed from Dr. Ting's
+            0: MEM(`XDA(s));
+            1: `SET(asel_n);
+            2: begin TOS(s); DW(0); end
+            endcase
+        ldi: 
             case (phase)
-            // 0: begin phase_n = 1; MEM(s); end
-            // 1: begin phase_n = 2; `HOLD; ALU(t + data); asel_n = 1'b1; end
-            // default: begin `PHASE0; `HOLD; TOS(s); DW(0); end
-            // CC: change Dr. Ting's logic
-            0: begin MEM(`XDA(s)); end
-            1: begin `SET(asel_n); end
-            default: begin TOS(s); DW(0); end
-            endcase // case (phase)
-        ldi:
-            case (phase)
-            0: PUSH(`X8D(data));
             1: TOS(t_d);
             2: TOS(t_d);
             3: TOS(t_d);
             endcase
         get:
             case (phase)
-            0: begin MEM(ibuf); `S(sPUSH); end
+            0: MEM(ibuf);
             1: begin TOS(`X8D(data)); `SET(ibuf_x); end
             endcase
         put:
             case (phase)
             0: begin MEM(obuf); `SET(dsel_x); end
-            default: begin POP(); DW(3); `SET(obuf_x); end
+            1: begin DW(3);     `SET(obuf_x); end
             endcase
         endcase
     end // always_comb
@@ -194,10 +166,7 @@ module EJ32_LS #(
             a     <= {ASZ{1'b0}};
         end
         else if (ctl.clk && ls_en) begin
-            ctl.phase <= phase_n;
-            asel      <= asel_n;
-            // instruction
-            if (code_x)  ctl.code <= code_n;
+            asel <= asel_n;
             if (t_x)     ctl.t    <= t_n;
             if (a_x)     a        <= a_n;
             if (dsel_x)  dsel     <= dsel_n;
