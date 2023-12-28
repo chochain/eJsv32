954070848363672f122bac3cbea0a3c18f607a24
gnii <chochain@yahoo.com>
2023-12-28 15:50:12 +0800

    ~/src/eJ32_br.sv drop code_x,phase_n; mv ss_op to AU

--- source/eJ32_br.sv
+++ source/eJ32_br.sv
@@ -3,20 +3,20 @@
 //
 `include "../source/eJ32_if.sv"
 
-`define PHASE0 phase_n = 0
 `define S(op)  ctl.ss_op = op
 `define R(op)  ctl.rs_op = op
 
 module EJ32_BR #(
-    parameter DSZ      = 32,              ///> 32-bit data width
-    parameter ASZ      = 17,              ///> 128K address space
-    parameter RS_DEPTH = 32               ///> 32 deep return stack
+    parameter DSZ      = 32,    ///> 32-bit data width
+    parameter ASZ      = 17,    ///> 128K address space
+    parameter RS_DEPTH = 32     ///> 32 deep return stack
     ) (
     EJ32_CTL ctl,
-    input  `U1 br_en,                     ///> branching unit active
-    input  `U8 data,                      ///> data from memory bus
-    input  `DU s,                         ///> NOS from stack unit
-    output `IU br_addr_o                  ///> address to memory bus
+    input  `U1 br_en,           ///> branching unit active
+    input  `U8 data,            ///> data from memory bus
+    input  `DU s,               ///> NOS from stack unit
+    output `IU br_p_o,
+    output `IU br_addr_o
     );
     import ej32_pkg::*;
     /// @defgroup Registers
@@ -26,17 +26,16 @@ module EJ32_BR #(
     `IU  p, a;                  ///> program counter, instruction pointer
     `U1  asel;                  ///> address bus mux (P|A)
     // return stack
-    `DU  r;                     ///> top of RS
     `DU  rs[RS_DEPTH];          ///> return stack, 3K LUTs, TODO: use EBR memory
+    `DU  r;                     ///> top of RS
     `U5  rp;                    ///> return stack pointers
     // IO
     /// @}
     /// @defgroup Next Register
     /// @{
     // instruction
-    opcode_t code_n;            ///> JVM opcode
-    `U3 phase_n;                ///> FSM phase (aka state)
-    `IU p_n, a_n;               ///> program counter, instruction pointer
+    `IU p_n;                    ///> program counter
+    `IU a_n;                    ///> instruction pointer
     `U1 asel_n;                 ///> address bus mux (P|A)
     // data & return stacks
     `DU t_n, r_n;               ///> TOS, top of return stack
@@ -45,7 +44,6 @@ module EJ32_BR #(
     /// @{
     // instruction
     opcode_t code;              ///> shadow ctl.code
-    `U1 code_x;                 ///> instruction unit control
     `U1 p_x, a_x;               ///> address controls
     `IU a_d;                    ///> 2-byte merged address
     // data stack
@@ -53,14 +51,12 @@ module EJ32_BR #(
     `U1 t_x, t_z, t_neg;        ///> TOS controls
     `DU t_d;                    ///> 4-byte merged data
     // return stack
-    `U5 rp1;                    ///> return stack pointers
-    `U1 r_x;                    ///> return stack controls
+    `U5 rp1;                    ///> return stack pointer
+    `U1 r_x;                    ///> return stack control
 
     // data stack
     task TOS(input `DU v);  t_n = v;  `SET(t_x); endtask;
-    task ALU(input `DU v);  TOS(v);   `S(sPOP);  endtask;
     task PUSH(input `DU v); TOS(v);   `S(sPUSH); endtask;
-    task POP();             TOS(s);   `S(sPOP);  endtask;
     task RPUSH(input `DU v); r_n = v; `R(sPUSH); endtask;
     // branching
     // Note: address is memory offset (instead of Java class file reference)
@@ -70,11 +66,11 @@ module EJ32_BR #(
     task BRAN(input `U1 f);
         case (phase)
         0: SETA(data);
-        1: begin POP(); if (f) JMP(a_d); end
+        1: if (f) JMP(a_d);
         endcase
     endtask: BRAN
     // memory unit
-    task MEM(input `IU a); SETA(a); `SET(asel_n); endtask;  // fetch from memory, data returns next cycle
+    task MEM(input `IU a); SETA(a); `SET(asel_n); endtask; // fetch from memory, data returns next cycle
     ///
     /// wires to reduce verbosity
     ///
@@ -88,15 +84,13 @@ module EJ32_BR #(
     assign t      = ctl.t;
     assign t_z    = ctl.t_z;
     assign t_neg  = ctl.t_neg;
-    assign addr_o = (asel) ? a : p;           ///> address, data or instruction
     assign t_d    = {t[DSZ-9:0], data};       ///> merge lowest byte into t
     assign a_d    = {a[ASZ-9:0], data};       ///> merge lowest byte into addr
+    assign br_addr_o = (asel) ? a : p;        ///> address, data or instruction
     ///
     /// combinational
     ///
     task INIT();
-        code_x    = 1'b1;         /// fetch opcode by default
-        phase_n   = 3'b0;         /// phase and IO controls
         p_n       = p + 'h1;      /// advance program counter
         p_x       = 1'b1;         /// advance PC by default
         a_n       = {ASZ{1'b0}};  /// default to clear address
@@ -116,7 +110,7 @@ module EJ32_BR #(
         iload_1: PUSH(rs[rp - 1]);       // CC: not tested
         iload_2: PUSH(rs[rp - 2]);       // CC: not tested
         iload_3: PUSH(rs[rp - 3]);       // CC: not tested
-        istore_0: begin r_n = t; `R(sMOVE); POP(); end  // local var, CC: not tested
+        istore_0: begin r_n = t; `R(sMOVE); end  // local var, CC: not tested
         //
         // conditional branching ops
         // Logical ops
@@ -155,7 +149,6 @@ module EJ32_BR #(
             case (phase)
             0: begin SETA(`X8A(data)); RPUSH(`XAD(p) + 2); end
             1: begin JMP(a_d); end
-            default: `PHASE0;
             endcase
         donext:
             case (phase)
@@ -168,40 +161,29 @@ module EJ32_BR #(
             endcase
         dupr: PUSH(r);
         popr: begin PUSH(r); `R(sPOP); end
-        pushr:begin POP(); RPUSH(t);   end
+        pushr:RPUSH(t);
         endcase
     end
-    ///
-    /// data & return stacks
-    ///
-    always_ff @(posedge ctl.clk, posedge ctl.rst) begin
-        if (ctl.rst) begin
-            rp <= '0;
-        end
-        else if (ctl.clk && br_en) begin
-            // return stack
-            case (ctl.rs_op)
-            sMOVE: rs[rp] <= r_n;
-            sPOP:  rp <= rp - 1;
-            sPUSH: begin rs[rp1] <= r_n; rp <= rp + 1; end
-            endcase
-        end
-    end // always_ff @ (posedge ctl.clk, posedge ctl.rst)
 
     always_ff @(posedge ctl.clk, posedge ctl.rst) begin
         if (ctl.rst) begin
-            asel <= 1'b0;
-            a    <= {ASZ{1'b0}};
             p    <= {ASZ{1'b0}};
+            a    <= {ASZ{1'b0}};
+            asel <= 1'b0;
+            rp   <= '0;
         end
         else if (ctl.clk && br_en) begin
-            ctl.phase <= phase_n;
-            asel      <= asel_n;
-            // instruction
-            if (code_x) ctl.code <= code_n;
-            if (t_x)    ctl.t    <= t_n;
-            if (a_x)    a        <= a_n;
-            if (p_x)    p        <= p_n;
+            asel <= asel_n;
+            // instruction pointer
+            if (t_x) ctl.t <= t_n;
+            if (a_x) a     <= a_n;
+            if (p_x) p     <= p_n;
+            // return stack
+            case (ctl.rs_op)
+            sMOVE: rs[rp] <= r_n;
+            sPOP:  rp     <= rp - 1;
+            sPUSH: begin rs[rp1] <= r_n; rp <= rp + 1; end
+            endcase
         end
-    end // always_ff @ (posedge clk, posedge rst)
+    end
 endmodule: EJ32_BR
