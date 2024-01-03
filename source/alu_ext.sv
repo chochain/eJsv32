ce49818976531be233c4cc82e53feb43031a8a95
gnii <chochain@yahoo.com>
2024-01-03 17:34:02 +0800

    ~/src/alu_ext.sv use common size; fix div (Dr. Ting)

--- source/alu_ext.sv
+++ source/alu_ext.sv
@@ -1,18 +1,23 @@
 ///
 /// @file
-/// @brief - ALU extended funtions
+/// @brief - ALU external modules
 ///    shifter    - shifter (both directions)
 ///    ushifter   - unsigned shifter
 ///    multiplier - 64-bit = 32x32
-///    divider    -
+///    divider    - 32/32-bit divider
 ///
+`include "../source/eJ32.vh"
+import ej32_pkg::*;
+
+`define DX logic[DSZ:0]        /* 1-bit wider */
+
 module shifter #(
     parameter DSZ = 32
     )(
-	input	[DSZ-1:0] d,
-	input	          dir,
-	input	[4:0]     bits,
-	output	[DSZ-1:0] r
+	input	`DU  d,
+	input	`U1  dir,
+	input	`U5  bits,
+	output	`DU  r
     );
     assign r = dir ? d << bits : d >> bits;
 endmodule
@@ -20,60 +25,64 @@ endmodule
 module ushifter #(
     parameter DSZ = 32
     )(
-	input	[DSZ-1:0]  d,
-	input	[4:0]      bits,
-	output	[DSZ-1:0]  r
+	input	`DU  d,
+	input	`U5  bits,
+	output	`DU  r
     );
     assign r = $unsigned(d) >> bits;
 endmodule
 
 module mult #(
-    parameter DSZ = 32,
-    parameter QSZ = DSZ*2
-    )(
-	input  [DSZ-1:0] a, b,
-	output [QSZ-1:0] r
+    parameter DSZ = 32
+    ) (
+	input  `DU  a,
+    input  `DU  b,
+	output `DU2 r
 	);
     assign r = a * b;
 endmodule
 
 module div_int #(parameter DSZ=32) (
-    input  logic clk,
-    input  logic rst,                 // start signal
-    input  logic [DSZ-1:0] x,         // dividend
-    input  logic [DSZ-1:0] y,         // divisor
-    output logic busy,                // calculation in progress
-    output logic dbz,                 // divide by zero flag
-    output logic [DSZ-1:0] q,         // quotient
-    output logic [DSZ-1:0] r          // remainder
+    input  `U1  clk,
+    input  `U1  rst,       // start signal
+    input  `DU  x,         // dividend
+    input  `DU  y,         // divisor
+    output `U1  busy,      // calculation in progress
+    output `U1  dbz,       // divide by zero flag
+    output `DU  q,         // quotient
+    output `DU  r          // remainder
     );
-    logic [DSZ-1:0] _q;               // intermediate quotient
-    logic [DSZ:0]   _r, r1;           // accumulator (1 bit wider)
     logic [$clog2(DSZ)-1:0] i;        // iteration counter
+    `DX r_n;               // accumulators (1 bit wider) 
+    `DX r1;
+    `DX ry;       
+    `DU q_n;               // intermediate quotient
+    `U1 pos;
 
+    assign pos = r1 >= {1'b0, y};
+    assign ry  = r1 - y;
+   
     always_comb begin
-        if (r1 >= {1'b0, y}) begin
-            {_r, _q} = {{r1 - y}[DSZ-1:0], q, 1'b1}; // 65-bit ops
-        end
-        else {_r, _q} = {r1[DSZ-1:0], q, 1'b0};
+        if (pos) {r_n, q_n} = {ry[DSZ-1:0], q, 1'b1}; // 65-bit ops
+        else     {r_n, q_n} = {r1[DSZ-1:0], q, 1'b0};
     end
 
     always_ff @(posedge clk) begin
         if (rst) begin
-            i      <= 1;
-            busy   <= 1'b1;
-            dbz    <= y == 0;
-            {r1, q} <= {{(DSZ-1){1'b0}}, x, 1'b0};   // shift left with carry
+            i    <= 1;
+            q    <= 0;
+            busy <= 1'b1;
+            dbz  <= y == 0;
+            {r1, q}  <= {{DSZ{1'b0}}, x, 1'b0};   // shift left with carry
         end
         else if (busy) begin
-            if (i) r1 <= _r;           // next digit
-            else begin                 // last bit
+            if (i==0) begin            // last bit
                 busy <= 1'b0;          // we are done
-                r    <= _r[DSZ:1];     // undo final shift
+                r    <= r_n[DSZ:1];    // undo final shift
             end
-            q <= _q;
+            else r1 <= r_n;            // next digit
+            q <= q_n;
             i <= i + 1;
-            //$write("[%d] r1.q _r_q %9x.%8x %9x_%8x ", i, r1, q, _r, _q);
         end
     end
 endmodule: div_int
