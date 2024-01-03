8c470e18d5d9b02ef9f4c7e1515815d0dc429006
gnii <chochain@yahoo.com>
2024-01-03 09:38:49 +0800

    ~/src/eJ32.sv add comment to signals

--- source/eJ32.sv
+++ source/eJ32.sv
@@ -15,21 +15,20 @@ module EJ32 #(
     parameter SS_DEPTH = 32,    ///> data stack depth
     parameter RS_DEPTH = 32     ///> return stack depth
     );
-    `U1  au_en, br_en, ls_en;
-    // ej32 memory bus
-    `IU  p;
-    `U8  data;
-    `DU  s;
-    `U1  p_inc;
-    `U1  div_bsy_o;
-    `IU  br_p_o;
-    `U1  br_psel;
+    `U1  au_en, br_en, ls_en;   ///> unit enables
+    `IU  p;                     ///> program counter
+    `U8  data;                  ///> data return from memory bus
+    `DU  s;                     ///> NOS
+    `U1  p_inc;                 ///> program counter advance flag
+    `U1  div_bsy_o;             ///> AU divider busy flag
+    `IU  br_p_o;                ///> BR branching target
+    `U1  br_psel;               ///> BR branching target select
 
     mb8_io      b8_if();                         ///> 8-bit memory bus
     spram8_128k smem(b8_if.slave, ~ctl.clk);     ///> tick on neg cycle
 
     EJ32_CTL    ctl();                           ///> ej32 control bus
-    EJ32_DC     #(COLD) dc(.div_bsy(div_bsy_o), .*);
+    EJ32_DC     dc(.div_bsy(div_bsy_o), .*);     ///> decoder
     EJ32_AU     #(SS_DEPTH, DSZ)       au(.s_o(s), .*);
     EJ32_BR     #(RS_DEPTH, DSZ, ASZ)  br(.*);
     EJ32_LS     #(TIB, OBUF, DSZ, ASZ) ls(.*);
