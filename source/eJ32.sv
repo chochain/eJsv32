2a88287c71a133ec9ee1b12451ccfd1d1cf81789
gnii <chochain@yahoo.com>
2024-01-23 16:28:44 +0800

    ~/src/eJ32.sv add DP unit

--- source/eJ32.sv
+++ source/eJ32.sv
@@ -16,18 +16,21 @@ module EJ32 #(
     input `U1 clk, rst /*synthesis syn_force_pads=1 syn_noprune=1*/
     );
     `U1  au_en, br_en, ls_en;   ///> unit enables
-    `U1  dc_en, rom_en;
+    `U1  dc_en, dp_en, rom_en;
     `IU  p, p_n;                ///> program counter
     `U8  rom_wait;              ///> ROM wait cycles for EBR to stablize read
     `IU  rom_a;                 ///> ROM address pointer
     `U8  rom_d, data;           ///> data return from memory bus
     `U1  p_inc;                 ///> program counter advance flag
-    `U1  div_bsy_o;             ///> AU divider busy flag
+    `U1  dp_bsy_o;              ///> Data processor/divider busy flag
     `DU  s_o;                   ///> NOS (AU -> LS)
     `IU  br_p_o;                ///> BR branching target
     `U1  br_psel;               ///> BR branching target select
-    `DU  au_t_o, br_t_o, ls_t_o;///> TOS from modules for arbitration
-    `U1  au_t_x, br_t_x, ls_t_x;
+    ///
+    /// TOS from modules for arbitration
+    ///
+    `DU  au_t_o, br_t_o, ls_t_o, dp_t_o;
+    `U1  au_t_x, br_t_x, ls_t_x, dp_t_x;
     ///
     /// EJ32 buses
     ///
@@ -41,8 +44,9 @@ module EJ32 #(
     ///
     /// EJ32 core modules
     ///
-    EJ32_DC     dc(.div_bsy(div_bsy_o), .*);     ///> decoder unit
-    EJ32_AU     au(.*);                          ///> arithmetic unit
+    EJ32_DC     dc(.div_bsy(dp_bsy_o), .*);      ///> decoder unit
+    EJ32_AU     au(.div_bsy(dp_bsy_o), .*);      ///> arithmetic unit
+    EJ32_DP     dp(.s(s_o), .*);                 ///> data processor/divider module
     EJ32_BR     br(.*);                          ///> branching unit
     EJ32_LS     #(TIB, OBUF) ls(.s(s_o), .*);    ///> load/store unit
     ///
@@ -69,26 +73,19 @@ module EJ32 #(
     /// TOS update arbitrator
     ///
     task UPDATE_TOS();
-        automatic `U3 sel = { au_t_x, br_t_x, ls_t_x };
+        automatic logic[3:0] sel = { au_t_x, br_t_x, ls_t_x, dp_t_x };
         case (sel)
-        3'b000: begin end // OK, ctl.t stays the same
-        3'b100: ctl.t <= au_t_o;
-        3'b010: ctl.t <= br_t_o;
-        3'b001: ctl.t <= ls_t_o;
+        4'b0000: begin end // OK, ctl.t stays the same
+        4'b1000: ctl.t <= au_t_o;
+        4'b0100: ctl.t <= br_t_o;
+        4'b0010: ctl.t <= ls_t_o;
+        4'b0001: ctl.t <= dp_t_o;
         default: $display("TOS Arbiter ERR t_x=%x", sel);
         endcase
     endtask: UPDATE_TOS
     ///
     /// Instruction Unit
     ///
-/* iCE40up5k high speed clock (48MHz) in built-in library
-    `U1 clk0;
-    HSOSC #(.CLKHF_DIV("b01")) clock(            // 48MHz divide by 2
-    .CLKHFEN(1'b1),                              // Enable the output  
-    .CLKHFPU(1'b1),                              // Power up the oscillator  
-    .CLKHF(clk)                                  // Oscillator output  
-    );
-*/   
     assign ctl.clk = clk;                        ///> clock driver
     assign ctl.rst = rst;
     assign p       = br_psel ? br_p_o : p_n;     ///> branch target
